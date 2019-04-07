d5e2769317870acd616d731419bd1441d6c24e16
prasadchandrasekaran <prasadvit@gmail.com>
2019-04-06 20:07:53 -0600

    project zero

---
+++ src/test/java/com/projectzero/ProjectzeroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projectzero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectzeroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
